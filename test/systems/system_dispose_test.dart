cf187e7611b88304fcf37127abcad64aa71f9735
beeth0ven <beeth0vendev@gmail.com>
2021-06-14 10:20:04 +0800

    add tests `System.dispose`

---
+++ test/systems/system_dispose_test.dart
@@ -0,0 +1,20 @@
+import 'package:test/test.dart';
+import 'package:love/love.dart';
+
+void main() {
+
+  test('System.dispose', () async {
+    int disposeCounts = 0;
+    final system = System<String, String>
+      .pure(({effect, reduce}) {
+        return Dispose(() => disposeCounts += 1);
+      });
+
+    final dispose = system.run();
+    expect(disposeCounts, 0);
+    dispose();
+    expect(disposeCounts, 1);
+    dispose();
+    expect(disposeCounts, 1);
+  });
+}
