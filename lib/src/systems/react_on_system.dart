bbac764137b2ff196e393f2f8399ef3e758dbde8
beeth0ven <beeth0vendev@gmail.com>
2021-06-16 09:22:05 +0800

    add operator `System.reactLatest`

--- lib/src/systems/react_on_system.dart
+++ lib/src/systems/react_on_system.dart
@@ -1,6 +1,7 @@
 import 'system.dart';
 import '../types/types.dart';
 import '../types/optional.dart';
+import '../types/latest_context.dart';
 
 extension ReactOperators<State, Event> on System<State, Event> {
 
@@ -38,6 +39,32 @@ extension ReactOperators<State, Event> on System<State, Event> {
     effect: effect,
   );
 
+  /// Add `effect` triggered by react partial state value change,
+  /// it will cancel previous effect when value changed.
+  /// 
+  /// [value] describe which part of value is observed.
+  /// 
+  /// [areEqual] describe how old value and new value are treat as equal (not change).
+  /// 
+  /// [skipFirstValue] is false if first value triggers the effect, 
+  /// is ture if first value won't trigger effect, default is false.
+  /// 
+  /// [effect] describe side effect, if effect has cancellation mechanism,
+  /// We can return a `Dispose` function contain the cancellation logic in effect callback.
+  /// This `Dispose` will be called when value changed or system dispose is called.
+  /// 
+  System<State, Event> reactLatest<Value>({
+    required Value Function(State state) value,
+    AreEqual<Value>? areEqual,
+    bool skipFirstValue = false,
+    required Dispose? Function(Value value, Dispatch<Event> dispatch) effect,
+  }) => _reactLatestRequest(
+    test: (state) => OptionalValue(value(state)),
+    areEqual: areEqual,
+    skipFirstReqeust: skipFirstValue,
+    effect: effect,
+  );
+
   System<State, Event> _reactRequest<Request>({
     required Optional<Request> Function(State state) test,
     AreEqual<Request>? areEqual,
@@ -67,6 +94,43 @@ extension ReactOperators<State, Event> on System<State, Event> {
       }
     },
   );
+
+  System<State, Event> _reactLatestRequest<Request>({
+    required Optional<Request> Function(State state) test,
+    AreEqual<Request>? areEqual,
+    bool skipFirstReqeust = false,
+    required Dispose? Function(Request request, Dispatch<Event> dispatch) effect,
+  }) => withContext<_LatestRequestContext<Request, Event>>(
+    createContext: () => _LatestRequestContext(
+      requestContext: _RequestContext(
+        skipRequestOnce: skipFirstReqeust,
+      ),
+      latestContext: LatestContext(),
+    ),
+    effect: (context, state, oldState, event, dispatch) {
+      final latestContext = context.latestContext;
+      final requestContext = context.requestContext;
+      final request = test(state);
+      final oldRequest = requestContext.oldRequest;
+      final changed = _optinalChanged(
+        oldValue: oldRequest,
+        value: request,
+        areEqual: areEqual,
+      );
+      if (changed) {
+        requestContext.oldRequest = request;
+        latestContext.disposePreviousEffect();
+        if (request is OptionalValue<Request>) {
+          if (requestContext.skipRequestOnce) {
+            requestContext.skipRequestOnce = false;
+            return;
+          }
+          latestContext.dispose = effect(request.value, latestContext.versioned(dispatch));
+        }
+      }
+    },
+    dispose: (context) => context.latestContext.disposePreviousEffect(),
+  );  
 }
 
 class _RequestContext<Request, Event> {
@@ -75,6 +139,15 @@ class _RequestContext<Request, Event> {
   });
   bool skipRequestOnce;
   Optional<Request> oldRequest = OptionalNone();
+}
+
+class _LatestRequestContext<Request, Event> {
+  _LatestRequestContext({
+    required this.requestContext,
+    required this.latestContext,
+  });
+  final _RequestContext<Request, Event> requestContext;
+  final LatestContext<Event> latestContext;
 }
 
 bool _optinalChanged<Value>({
