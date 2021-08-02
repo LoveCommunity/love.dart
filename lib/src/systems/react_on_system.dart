cc7ce4a6c697b02fc077d66d68d366382b723c96
beeth0ven <beeth0vendev@gmail.com>
2021-08-02 09:24:38 +0800

    remove `system.reactRequest` operator for simplicity

--- lib/src/systems/react_on_system.dart
+++ lib/src/systems/react_on_system.dart
@@ -53,34 +53,6 @@ extension ReactOperators<State, Event> on System<State, Event> {
     effect: effect,
   );
 
-  /// Add `effect` triggered by a request which is computed from state.
-  /// 
-  /// Every time state changed, a request is computed from state, 
-  /// if a fresh request is computed, the effect will be triggered by this request.
-  /// Fresh request means newRequest is not null and newRequest are not equal to oldRequest. 
-  /// 
-  /// [request] describe how request is computed from state. 
-  /// return null if there is no request.
-  /// 
-  /// [areEqual] describe how old request and new request are treat as equal (not change).
-  /// 
-  /// [skipFirstRequest] is false if first request will trigger the effect, 
-  /// is ture if first request won't trigger effect, default is false.  
-  /// 
-  /// [effect] describe side effect.
-  ///  
-  System<State, Event> reactRequest<Request>({
-    required Request? Function(State state) request,
-    AreEqual<Request>? areEqual,
-    bool skipFirstRequest = false,
-    required void Function(Request request, Dispatch<Event> dispatch) effect,
-  }) => _reactRequest(
-    test: (state) => _toOptional(request(state)),
-    areEqual: areEqual,
-    skipFirstRequest: skipFirstRequest,
-    effect: effect,
-  );
-
   /// Add `effect` triggered by a request which is computed from state,
   /// it will cancel previous effect when request changed.
   /// 
