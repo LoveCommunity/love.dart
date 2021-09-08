086321e3f049aaa714401315647d67ba7142017a
beeth0ven <beeth0vendev@gmail.com>
2021-09-08 08:41:36 +0800

    add `system.log` operator code docs

--- lib/src/systems/log_on_system.dart
+++ lib/src/systems/log_on_system.dart
@@ -5,6 +5,86 @@ import 'dart:core' as core; // resolve conflict with parameter 'system.log.print
 
 extension LogOperator<State, Event> on System<State, Event> {
 
+  /// Add log `effect` to the system.
+  /// 
+  /// After apply this operator, system will print message when:
+  /// * system run
+  /// * system update state
+  /// * system dispose
+  /// 
+  /// For example:
+  /// 
+  /// ```dart
+  /// System<int, CounterEvent> createCounterSystem() { ... }
+  ///
+  /// void main() async {
+  /// 
+  ///   final counterSystem = createCounterSystem();
+  /// 
+  ///   final dispose = counterSystem
+  ///     .log()  // --> add log effect
+  ///     .run();
+  ///   
+  ///   await Future<void>.delayed(Duration(seconds: 3));
+  /// 
+  ///   dispose();
+  /// 
+  /// } 
+  /// ```
+  /// 
+  /// will log something like this:
+  /// 
+  /// ```
+  /// System<int, CounterEvent> Run
+  /// System<int, CounterEvent> Update {
+  ///   event: null
+  ///   oldState: null
+  ///   state: 0
+  /// }
+  /// System<int, CounterEvent> Update {
+  ///   event: Instance of 'CounterEventIncrease'
+  ///   oldState: 0
+  ///   state: 1
+  /// }
+  /// System<int, CounterEvent> Dispose
+  /// ```
+  /// If we want to replace the runtimeType `System<int, CounterEvent>` text with some 
+  /// meaningful [label], we can use [label] parameter:
+  /// 
+  /// ```dart
+  ///   counterSystem
+  ///     .log(label: 'CounterSystem')
+  ///     .run();
+  /// ```
+  /// 
+  /// the output will become:
+  /// 
+   /// ```
+  /// CounterSystem Run
+  /// CounterSystem Update {
+  ///   event: null
+  ///   oldState: null
+  ///   state: 0
+  /// }
+  /// CounterSystem Update {
+  ///   event: Instance of 'CounterEventIncrease'
+  ///   oldState: 0
+  ///   state: 1
+  /// }
+  /// CounterSystem Dispose
+  /// ```
+  /// 
+  /// By default `log` operator use built in `print` function (from ''dart:core'') to print message, 
+  /// but we can provide custom [print] function (eg, `debugPrint`, `logger.log`) to replace it:
+  /// 
+  /// ```dart
+  ///   counterSystem
+  ///     .log(print: (message) => debugPrint(message))
+  ///     .run();
+  /// ```
+  /// 
+  /// The message will be print with `debugPrint` in the above example.
+  /// 
   System<State, Event> log({
     core.String? label,
     void Function(core.String message)? print,
